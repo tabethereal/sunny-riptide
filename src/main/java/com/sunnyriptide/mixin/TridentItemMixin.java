1a9e3d117b9dcffb3b8071fbb5b5b85f6c3e757f
tableau ethereal <145373380+tabethereal@users.noreply.github.com>
2025-09-04 20:19:57 +0800

    Initial commit

---
+++ src/main/java/com/sunnyriptide/mixin/TridentItemMixin.java
@@ -0,0 +1,20 @@
+package com.sunnyriptide.mixin;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.TridentItem;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(TridentItem.class)
+public class TridentItemMixin {
+    @Redirect(method = "use", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;isTouchingWaterOrRain()Z"))
+    private boolean useAlwaysTouchingWaterOrRain(PlayerEntity instance) {
+        return true;
+    }
+
+    @Redirect(method = "onStoppedUsing", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;isTouchingWaterOrRain()Z"))
+    private boolean releaseAlwaysTouchingWaterOrRain(PlayerEntity instance) {
+        return true;
+    }
+}
